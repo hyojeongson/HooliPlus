26445519e57bf80ecd61941f6c02e614850bcc6c
daniel kwangho Suk <khseok1060@gmail.com>
2017-02-04 16:06:20 +0900

    Client index.html setting

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,11 @@
 angular.module('HooliPlus', [
-
-
+  'ngMaterial',
+  'ngRoute',
+  'ngMessages'
   ])
+
+.config(function ($routeProvider, $httpProvider, $locationProvider) {
+  
+  $locationProvider.hashPrefix('');
+
+})
