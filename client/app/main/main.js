cf0270d328acab3cd654c3dc60a46b56a2b68287
hyojeong Son <web.sonhyoj@gmail.com>
2017-02-06 15:54:12 +0900

    mainbar update

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,5 +1,14 @@
 angular.module('HooliPlus.main', [])
 
-.controller('MainController', function ($scope) {
-	
+.controller('MainController', function ($scope, $timeout, $mdSidenav) {
+	$scope.toggleLeft = buildToggler('left');
+  $scope.toggleRight = buildToggler('right');
+
+
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      };
+    }
 })
+
