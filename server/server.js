192e8067d7e29e701de9aee2ccb4f7b7c32ed5e3
SeongChan Choi <seongchanchoi@SeongChans-MacBook-Pro.local>
2017-02-07 14:02:29 +0900

    pull main and login dialog

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express();
+var server = require('http').createServer(app); 
+var io = require('socket.io')(server);
 
 // connect to mongo database named "hooliplus"
 mongoose.connect('mongodb://localhost/hooliplus');
@@ -11,7 +13,16 @@ require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
-app.listen(8000);
+server.listen(8000); //app.listen
+
+// socket io connection
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
