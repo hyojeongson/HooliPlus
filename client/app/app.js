4d6c51fedbcfa21af7f2a7653e10c7294666eeb3
JinHo Do <greatdjh@gmail.com>
2017-02-13 09:51:18 +0900

    (refactor) route config fix by imkojs

--- client/app/app.js
+++ client/app/app.js
@@ -15,16 +15,8 @@ angular.module('HooliPlus', [
       templateUrl: 'app/main/main.html', // 기본 레이아웃
       controller: 'MainController'
     })
-    .when('/signin-popup', {
-      templateUrl: 'app/auth/signin-popup.html',
-      controller: 'AuthController'
-    })
-    .when('/signup-popup', {
-      templateUrl: 'app/auth/signup-popup.html',
-      controller: 'AuthController'
-    })
     .otherwise({
-      redirect: '/main.html'
+      redirectTo: '/'
     })
   
     // 이후 다른 라우트 자리 들
