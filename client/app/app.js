bf9b3a7184cd8b249d7c7e930f658f73bcb6c9c7
JinHo Do <greatdjh@gmail.com>
2017-02-13 09:23:35 +0900

    add otherwise

--- client/app/app.js
+++ client/app/app.js
@@ -15,13 +15,16 @@ angular.module('HooliPlus', [
       templateUrl: 'app/main/main.html', // 기본 레이아웃
       controller: 'MainController'
     })
-    .when('/signin', {
-      templateUrl: 'app/auth/signin.html',
+    .when('/signin-popup', {
+      templateUrl: 'app/auth/signin-popup.html',
       controller: 'AuthController'
     })
-    .when('/signup', {
-      templateUrl: 'app/auth/signup.html',
+    .when('/signup-popup', {
+      templateUrl: 'app/auth/signup-popup.html',
       controller: 'AuthController'
+    })
+    .otherwise({
+      redirect: '/main.html'
     })
   
     // 이후 다른 라우트 자리 들
